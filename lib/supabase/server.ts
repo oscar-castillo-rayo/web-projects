b5b2e32c2a673ddf4400f1858fc60b182b4bd314
oscar-castillo-rayo <manuel.rayo3@gmail.com>
2025-05-19 01:37:49 -0600

    fix: server issue on import .env

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "@/lib/database.types";
-import "dotenv/config";
 
 export function createServerSupabaseClient() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
